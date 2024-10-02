94b461ce3c8e0a4c919a25fd332c837243eb0f47
IgnacioViadero <202206059@alu.comillas.edu>
2024-10-02 13:00:52 +0200

    primer repositorio

---
+++ PRACTICAS/PRACTICA 1 - Ignacio Viadero/ConsoleApp1/Taxi.cs
@@ -0,0 +1,54 @@
+﻿using Practica1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_1
+{
+    internal class Taxi : Vehicle
+    {
+        private bool has_passengers;
+        public Taxi(string plate) : base(plate, "Taxi", 0)
+        {
+            has_passengers = false;
+            SetVehicleSpeed(45);
+            Console.WriteLine($"{ToString()}: Created");
+        }
+        public bool Riding()
+        {
+            return has_passengers;
+        }
+        public void SetPassengers(bool passengers)
+        {
+            has_passengers = passengers;
+        }
+        public void StartRide()
+        {
+            string result = "";
+            if (Riding())
+            {
+                result = "is already in a ride.";
+            }
+            else
+            {
+                SetVehicleSpeed(100);
+                SetPassengers(true);
+                result = "starting a ride.";
+            }
+            Console.WriteLine($"{ToString()}: {result}");
+        }
+        public void StopRide()
+        {
+            string result = "";
+            if (Riding())
+            {
+                SetVehicleSpeed(45);
+                SetPassengers(false);
+                result = "Has finished the ride.";
+            }
+            Console.WriteLine($"{ToString()}: {result}");
+        }
+    }
+}
---
+++ PRACTICAS/PRACTICA 1 - Ignacio Viadero/ConsoleApp1/Vehicle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    internal class Vehicle
+    {
+        private string vehicle_type;
+        private string plate;
+        private int speed;
+
+        public Vehicle(string vehicle_type, string plate, int speed)
+        {
+            this.vehicle_type = vehicle_type;
+            this.plate = plate;
+            this.speed = speed;
+        }
+        public string GetVehicleType()
+        {
+            return vehicle_type;
+        }
+        public string GetVehiclePlate()
+        {
+            return plate;
+        }
+        public int GetVehicleSpeed()
+        {
+            return speed;
+        }
+        public override string ToString()
+        {
+            return $"{GetVehicleType()} with plate {GetVehiclePlate()}";
+        }
+        public void SetVehicleSpeed(int speed)
+        {
+            this.speed = speed;
+        }
+    }
+}
